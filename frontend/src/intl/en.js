01adb5c7b7c91b5438f7214b586cce31f082fa93
rhabikhalifa <rhabikhalifa@gmail.com>
2023-10-25 15:36:03 +0100

    add internationalisation

---
+++ frontend/src/intl/en.js
@@ -0,0 +1,10 @@
+const en = {
+    'landing.projects.myProjects': 'My projects',
+    'sidebar.projects': 'Projects',
+    'sidebar.dashboard': 'Dashboard',
+    'sidebar.editProject': 'Configuration',
+    'sidebar.templates': 'Templates',
+    'sidebar.authentification': 'Authentification',
+};
+
+export default en;
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
+import intlReducer from './intlReducer';
 import themeReducer from './themeReducer';
 
 const reducer = combineReducers({
-  theme: themeReducer
+  theme: themeReducer,
+  intl: intlReducer,
 });
 
 export default configureStore({
